462ee17b3b2434f2c8f0099e1a0a7ef2b72632b0
Olivier Duchaine <oduchaine@coveo.com>
2021-12-05 19:01:35 -0500

    provider definition and base k8s cluster definition

---
+++ main.tf
@@ -0,0 +1,31 @@
+terraform {
+  backend "s3" {
+    endpoint                    = "nyc3.digitaloceanspaces.com"
+    region                      = "us-west-1"
+    key                         = "terraform.tfstate"
+    bucket                      = "kube-terraform-state"
+
+    skip_credentials_validation = true
+    skip_metadata_api_check     = true
+  }
+}
+
+resource "digitalocean_kubernetes_cluster" "do-kubernetes-challenge" {
+  name   = "do-kubernetes-challenge"
+  region = "nyc3"
+  version = "1.21.5-do.0"
+
+  node_pool {
+    name       = "worker-pool"
+    size       = "s-2vcpu-2gb"
+    node_count = 2
+
+    taint {
+      key    = "workloadKind"
+      value  = "database"
+      effect = "NoSchedule"
+    }
+  }
+}
+
+
